a22c08cbf41215058d4384367ccad399bec19888
Kingsley Emeka <kingsley@staffbase.com>
2020-08-22 18:32:34 +0200

    :white_check_mark:test script updated

--- src/App.test.js
+++ src/App.test.js
@@ -6,6 +6,7 @@ import CreateQuestionForm from "./components/CreateQuestionForm";
 import CreateAnswerForm from "./components/CreateAnswerForm";
 
 afterEach(cleanup);
+
 describe("Question Form", () => {
   it("Ensures that  the correct content is rendered", () => {
     const { getByText, getByLabelText } = render(<CreateQuestionForm />);
@@ -17,16 +18,32 @@ describe("Question Form", () => {
     expect(getByText(/Submit Question/i)).not.toBeNull();
   });
 
-  it("checks if the input text updates the state", () => {
+  it("checks if the Name input text updates the state", () => {
     const { getByText, getByLabelText } = render(<CreateQuestionForm />);
     expect(getByText(/Name/i).textContent).toBe("Name:");
     fireEvent.change(getByLabelText("Name:"), { target: { value: "Text" } });
-    expect(getByText(/Name/i).textContent).toBe("Name:");
+    expect(getByText(/Name/i).textContent).not.toBe("Name: ");
+  });
+
+  it("checks if the Title input text updates the state", () => {
+    const { getByText, getByLabelText } = render(<CreateQuestionForm />);
+    expect(getByText(/Title/i).textContent).toBe("Title:");
+    fireEvent.change(getByLabelText("Title:"), { target: { value: "Text" } });
+    expect(getByText(/Title/i).textContent).not.toBe("Title: ");
+  });
+
+  it("checks if the Description input text updates the state", () => {
+    const { getByText, getByLabelText } = render(<CreateQuestionForm />);
+    expect(getByText(/Description/i).textContent).toBe("Description:");
+    fireEvent.change(getByLabelText("Description:"), {
+      target: { value: "Text" },
+    });
+    expect(getByText(/Description/i).textContent).not.toBe("Description: ");
   });
 });
 
 describe("Answer Form", () => {
-  it("Ensures that  the correct content is rendered", () => {
+  it("render the correct content", () => {
     const history = createMemoryHistory();
     const route = "/create-answer-to/:questionId";
     history.push(route);
@@ -36,8 +53,23 @@ describe("Answer Form", () => {
       </Router>
     );
 
-    expect(getByText(/Create a new answer/i)).not.toBeNull();
+    expect(getByText(/Create a new answer/i).textContent).toBe(
+      "Create a new answer"
+    );
     expect(getByLabelText(/Answer/i)).not.toBeNull();
     expect(getByText(/Submit Answer/i)).not.toBeNull();
+  });
+  it("Input text updates the state", () => {
+    const history = createMemoryHistory();
+    const route = "/create-answer-to/:questionId";
+    history.push(route);
+    const { getAllByText, getByLabelText } = render(
+      <Router history={history}>
+        <CreateAnswerForm />
+      </Router>
+    );
+    expect(getAllByText(/Answer/i).textContent).toBe("Answer");
+    fireEvent.change(getByLabelText("Answer:"), { target: { value: "Text" } });
+    expect(getAllByText(/Answer/i).textContent).not.toBe("Answer: ");
   });
 });
